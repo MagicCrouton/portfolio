b74c2385843cee4bac0a956c609671da566077c8
eugene park <e.park5336@gmail.com>
2023-03-26 18:50:10 -0400

    borrowed some code from class, and started modifying to use custom css sheet from my css portfolio and home page

---
+++ react-portfolio/src/components/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../css/homePage.css';
+import profilePic from '../images/profilePic.jpg'
+
+export default function Home() {
+  return (
+    <header>
+        <div class="profilePic">
+            <img src={profilePic} alt="Profile Picture"></img>
+        </div>
+        <div class="intro">
+            <h2 >Hello, I'm</h2>
+            <h1>Eugene Park</h1>
+            <h2>It may not be pretty</h2>
+            <h2>   but it works..</h2>
+        </div>
+        <ul>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </ul>
+    </header>
+  );
+}
