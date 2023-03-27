27afa6cfe3df66d18bf2b3f771433f147ed5c974
eugene park <e.park5336@gmail.com>
2023-03-26 21:59:07 -0400

    made a new react app because i npm fixed the file and the app is broken

---
+++ eugenepark-reactapp/src/components/PortfolioContainer.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // passed handPageChange to the homepage because it is used as a splash page and i wanted to hide the nav bar
+  const handlePageChange = (page) => setCurrentPage(page);
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home handlePageChange={handlePageChange} />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange}/>
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+    </div>
+  );
+}
