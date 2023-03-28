d388e238953a081a2c32bbd7df9e32ed454c894a
eugene park <e.park5336@gmail.com>
2023-03-27 20:48:28 -0400

    added footer to site need to fix icons

--- eugenepark-reactapp/src/components/PortfolioContainer.js
+++ eugenepark-reactapp/src/components/PortfolioContainer.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
+import Footer from './Footer'
 import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
-import './css/general.css'
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
@@ -34,6 +34,7 @@ export default function PortfolioContainer() {
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange}/>
       {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
+      <Footer currentPage={currentPage}/>
     </div>
   );
 }
